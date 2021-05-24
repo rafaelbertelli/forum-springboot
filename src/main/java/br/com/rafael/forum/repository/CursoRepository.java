275db57386eadb43664b67ecef0f826fb6894728
Rafael Borges <rafael.borges@luizalabs.com>
2021-05-24 19:27:14 -0300

    implements repository

---
+++ src/main/java/br/com/rafael/forum/repository/CursoRepository.java
@@ -0,0 +1,10 @@
+package br.com.rafael.forum.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.rafael.forum.modelo.Curso;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+  Curso findByNome(String nomeCurso);
+
+}
