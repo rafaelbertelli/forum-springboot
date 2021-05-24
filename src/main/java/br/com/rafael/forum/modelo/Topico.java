1c7b6dd1ef3e78c6e54f99c8adb605f777424dcf
Rafael Borges <rafael.borges@luizalabs.com>
2021-05-24 19:27:32 -0300

    implement model

--- src/main/java/br/com/rafael/forum/modelo/Topico.java
+++ src/main/java/br/com/rafael/forum/modelo/Topico.java
@@ -3,17 +3,34 @@ package br.com.rafael.forum.modelo;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
+@Entity
 public class Topico {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String titulo;
 	private String mensagem;
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+	@Enumerated(EnumType.STRING)
 	private StatusTopico status = StatusTopico.NAO_RESPONDIDO;
+	@ManyToOne
 	private Usuario autor;
+	@ManyToOne
 	private Curso curso;
+	@ManyToMany(mappedBy = "topico")
 	private List<Resposta> respostas = new ArrayList<>();
+
+	public Topico() {}
 
 	public Topico(String titulo, String mensagem, Curso curso) {
 		this.titulo = titulo;
