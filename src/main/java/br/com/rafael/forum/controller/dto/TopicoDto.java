b0a48b11085feeb4b6ccd627a45b4d9639b60ecc
Rafael Borges <rafael.borges@luizalabs.com>
2021-06-06 22:00:31 -0300

    refactor method to use Pageable interface

--- src/main/java/br/com/rafael/forum/controller/dto/TopicoDto.java
+++ src/main/java/br/com/rafael/forum/controller/dto/TopicoDto.java
@@ -1,8 +1,7 @@
 package br.com.rafael.forum.controller.dto;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.data.domain.Page;
 import br.com.rafael.forum.modelo.Topico;
 
 public class TopicoDto {
@@ -35,8 +34,8 @@ public class TopicoDto {
     return dataCriacao;
   }
 
-  public static List<TopicoDto> converter(List<Topico> topicos) {
-    return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+  public static Page<TopicoDto> converter(Page<Topico> topicos) {
+    return topicos.map(TopicoDto::new);
   }
 
 }
